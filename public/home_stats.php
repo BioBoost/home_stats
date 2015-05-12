7484f95afe3af17a4496d74af0b5da5f03e63210
Nico De Witte <nico.dewitte@vives.be>
2015-05-12 15:08:10 +0200

    Add Monolog

--- public/home_stats.php
+++ public/home_stats.php
@@ -1,3 +1,7 @@
+<?php
+    include 'vendor/autoload.php'
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,11 +11,25 @@
     <h1>Welcome to Home Stats</h1>
 
     <?php
-
         echo "GET:<br>";
         print_r($_GET);
         echo "<br><br><br>POST:<br>";
         print_r($_POST);
+    ?>
+
+    <?php
+        use Monolog\Logger;
+        use Monolog\Handler\StreamHandler;
+
+        // create a log channel
+        $log = new Logger('HomeStats');
+        $log->pushHandler(new StreamHandler('log/development.log', Logger::DEBUG));
+        $log->pushHandler(new StreamHandler('log/production.log', Logger::WARNING));
+
+        // add records to the log
+        $log->addDebug('D_Foo');
+        $log->warning('W_Bar');
+        $log->error('E_Kitty');
     ?>
 
 </body>
