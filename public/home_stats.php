90018a193e44b50e76416f39f8c734fe0f7933e8
Nico De Witte <nico.dewitte@vives.be>
2015-05-12 14:25:35 +0200

    Home page of Home Stats

---
+++ public/home_stats.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home Stats</title>
+</head>
+<body>
+    <h1>Welcome to Home Stats</h1>
+
+    <?php
+
+        echo "GET:<br>";
+        print_r($_GET);
+        echo "<br><br><br>POST:<br>";
+        print_r($_POST);
+    ?>
+
+</body>
+</html>
